b6635bc4f18227de28801317042ba0f33cadd987
Wellington Andrade <wmazoni@gmail.com>
2019-08-08 05:19:56 -0300

    Secao 3: Entrada de dados em C#

--- Course/Program.cs
+++ Course/Program.cs
@@ -3,24 +3,24 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            string produto1 = "Computador";
-            string produto2 = "Mesa de escritório";
-            byte idade = 30;
-            int codigo = 5290;
-            char genero = 'M';
-            double preco1 = 2100.0;
-            double preco2 = 650.50;
-            double medida = 53.234567;
-            Console.WriteLine("Produtos:");
-            Console.WriteLine("{0}, cujo preço é $ {1:F2}", produto1, preco1);
-            Console.WriteLine("{0}, cujo preco é $ {1:F2}", produto2, preco2);
-            Console.WriteLine();
-            Console.WriteLine("Registro: {0} anos de idade, código {1} e gênero: {2}", idade, codigo, genero);
-            Console.WriteLine();
-            Console.WriteLine("Medida com oito casas decimais: {0:F8}", medida);
-            Console.WriteLine("Arredondado (três casas decimais): {0:F3}", medida);
-            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Entre com o seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string nomeVet = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nomeVet);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
