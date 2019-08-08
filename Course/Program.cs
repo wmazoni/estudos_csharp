a9b69dd2cb8245c412f2073ba0b3637174696353
Wellington Andrade <wmazoni@gmail.com>
2019-08-08 05:38:26 -0300

    Secao 3: Exemplo de funcao

--- Course/Program.cs
+++ Course/Program.cs
@@ -3,24 +3,24 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Entre com o seu nome completo:");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Quantos quartos tem na sua casa?");
-            int quartos = int.Parse(Console.ReadLine());
-            Console.WriteLine("Entre com o preço de um produto:");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
-            string[] vet = Console.ReadLine().Split(' ');
-            string nomeVet = vet[0];
-            int idade = int.Parse(vet[1]);
-            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite três números:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            double resultado = Maior(n1, n2, n3);
+            Console.WriteLine("Maior = " + resultado);
+        }
+        static int Maior(int a, int b, int c) {
+            int m;
+            if (a > b && a > c) {
+                m = a;
+            } else if (b > c) {
+                m = b;
+            } else {
+                m = c;
+            }
+            return m;
 
-            Console.WriteLine(nome);
-            Console.WriteLine(quartos);
-            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine(nomeVet);
-            Console.WriteLine(idade);
-            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
