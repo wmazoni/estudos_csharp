a162957a7807f4e55336f596aaba1533b29a1f7c
Wellington <wmazoni@wmazoni.com>
2019-09-01 14:43:45 -0300

    Exercicio de fixacao sobre conjuntos

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,42 +1,38 @@
 ﻿using System;
-using System.Globalization;
 using System.Collections.Generic;
-
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            string[] line = Console.ReadLine().Split(' ');
-            int n = int.Parse(line[0]);
-            int m = int.Parse(line[1]);
-            int[,] mat = new int[n, m];
-            for (int i = 0; i < n; i++) {
-                string[] values = Console.ReadLine().Split(' ');
-                for (int j = 0; j < m; j++) {
-                    mat[i, j] = int.Parse(values[j]);
-                }
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+            Console.Write("Quantos alunos possui o curso A? ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso A: ");
+            for (int i = 0; i < N; i++) {
+                int x = int.Parse(Console.ReadLine());
+                A.Add(x);
             }
-            int x = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (mat[i,j] == x) {
-                        Console.WriteLine("Position " + i + "," + j + ":");
-                        if (j > 0) {
-                            Console.WriteLine("Left: " + mat[i,j - 1]);
-                        }
-                        if (i > 0) {
-                            Console.WriteLine("Up: " + mat[i - 1, j]);
-                        }
-                        if (j < m - 1) {
-                            Console.WriteLine("Right: " + mat[i, j + 1]);
-                        }
-                        if (i < n - 1) {
-                            Console.WriteLine("Down: " + mat[i + 1, j]);
-                        }
-                    }
-                }
+            Console.Write("Quantos alunos possui o curso B? ");
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso B: ");
+            for (int i = 0; i < N; i++) {
+                int x = int.Parse(Console.ReadLine());
+                B.Add(x);
             }
-            
+            Console.Write("Quantos alunos possui o curso C? ");
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso C: ");
+            for (int i = 0; i < N; i++) {
+                int x = int.Parse(Console.ReadLine());
+                C.Add(x);
+            }
+            HashSet<int> Novo = new HashSet<int>();
+            Novo.UnionWith(A);
+            Novo.UnionWith(B);
+            Novo.UnionWith(C);
+            Console.WriteLine("Total de Alunos: " + Novo.Count);
+            Console.ReadLine();
         }
     }
 }
