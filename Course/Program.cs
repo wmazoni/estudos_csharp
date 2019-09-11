d5dd4cee86b9f143fcdedde4fc1e4d54eb5b947d
Wellington <wmazoni@wmazoni.com>
2019-09-10 23:27:58 -0300

    Exercicio de fixacao de vetor

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,26 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            double? x = null;
-            double? y = 10.0;
-
-            Console.WriteLine(x.GetValueOrDefault());
-            Console.WriteLine(y.GetValueOrDefault());
-
-            Console.WriteLine(x.HasValue);
-            Console.WriteLine(y.HasValue);
-
-            if (x.HasValue)
-                Console.WriteLine(x.Value);
-            else
-                Console.WriteLine("X is null");
-
-            if (y.HasValue)
-                Console.WriteLine(y.Value);
-            else
-                Console.WriteLine("Y is null");
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Students[] vect = new Students[10];
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Aluguel #"+i);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Students(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados");
+            for (int i = 0; i <= 9; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
         }
     }
 }
