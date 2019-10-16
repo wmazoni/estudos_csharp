02c9ee48ac58d366a3227745da1aca6e741b9f43
Wellington Andrade <wmazoni@wmazoni.com>
2019-10-16 20:08:10 -0300

    Funcoes para string

--- Course/Program.cs
+++ Course/Program.cs
@@ -4,37 +4,34 @@ using System.Collections.Generic;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Console.Write("How many employees will be registred? ");
-            int n = int.Parse(Console.ReadLine());
-            List<Employee> list = new List<Employee>();
-            for (int i = 1; i <= n; i++) {
-                Console.WriteLine("Employee #"+i);
-                Console.Write("Id: ");
-                int id = int.Parse(Console.ReadLine());
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                Console.Write("Salary: ");
-                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                list.Add(new Employee(id, name, salary));
-                Console.WriteLine();
-            }
-            
-            Console.Write("Enter the employee id that will have salary increase: ");
-            int searchID = int.Parse(Console.ReadLine());
-            Employee emp = list.Find(x => x.Id == searchID);
-            if (emp != null) {
-                Console.Write("Enter the percentage: ");
-                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                emp.IncreaseSalary(percentage);
-            }
-            else {
-                Console.WriteLine("This id does not exist!");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Updated list of employees:");
-            foreach (Employee obj in list) {
-                Console.WriteLine(obj);
-            }
+            string original = "abcde FGHIJ ABC abc DEFG     ";
+
+            string s1 = original.ToUpper();
+            string s2 = original.ToLower();
+            string s3 = original.Trim();
+
+            int n1 = original.IndexOf("bc");
+            int n2 = original.LastIndexOf("bc");
+
+            string s4 = original.Substring(3);
+            string s5 = original.Substring(3, 5);
+            string s6 = original.Replace('a', 'x');
+            string s7 = original.Replace("abc", "xy");
+
+            bool b1 = String.IsNullOrEmpty(original);
+
+
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-");
+            Console.WriteLine("ToLower: -" + s2 + "-");
+            Console.WriteLine("Trim: -" + s3 + "-");
+            Console.WriteLine("IndexOf (bc): " + n1);
+            Console.WriteLine("LastIndexOf: " + n2);
+            Console.WriteLine("Substring(3): " + s4);
+            Console.WriteLine("Substring(3,5): " + s5);
+            Console.WriteLine("Replace(a,x): " + s6);
+            Console.WriteLine("Replace(abc, xy: " + s7);
+            Console.WriteLine("IsNullOrEmpty: " + b1);
         }
     }
 }
