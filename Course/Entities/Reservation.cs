e7fe0d2366d95a78f8c197eb4c20d67ab0ee3210
Wellington Andrade <wmazoni@protonmail.com>
2020-01-10 05:57:30 -0300

    Criando solucoes personalizadas, solucao boa

--- Course/Entities/Reservation.cs
+++ Course/Entities/Reservation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Course.Entities.Exceptions;
 
 namespace Course.Entities
 {
@@ -18,6 +19,11 @@ namespace Course.Entities
         }
         public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
             RoomNumber = roomNumber;
             CheckIn = checkIn;
             CheckOut = checkOut;
@@ -27,23 +33,22 @@ namespace Course.Entities
             TimeSpan duration = CheckOut.Subtract(CheckIn);
             return (int)duration.TotalDays;
         }
-        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
         {
             DateTime now = DateTime.Now;
 
             if (checkIn < now || checkOut < now)
             {
-                return "Reservation dates for update must be future dates";
+                throw new DomainException("Reservation dates for update must be future dates");
             }
 
             if (checkOut <= checkIn)
             {
-                return "Check-out date must be after check-in date";
+                throw new DomainException("Check-out date must be after check-in date");
             }
 
             CheckIn = checkIn;
             CheckOut = checkOut;
-            return null;
         }
         public override string ToString()
         {
--- Course/Program.cs
+++ Course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Course.Entities;
+using Course.Entities.Exceptions;
 
 namespace Course
 {
@@ -8,19 +9,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.Write("Room number: ");
-            int number = int.Parse(Console.ReadLine());
-            Console.Write("Check-in date (dd/MM/yyyy): ");
-            DateTime checkIn = DateTime.Parse(Console.ReadLine());
-            Console.Write("Check-out date (dd/MM/yyyy): ");
-            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-            if (checkOut <= checkIn)
-            {
-                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
-            }
-            else
-            {
                 Reservation reservation = new Reservation(number, checkIn, checkOut);
                 Console.WriteLine("Reservation: " + reservation);
                 Console.WriteLine();
@@ -30,17 +27,21 @@ namespace Course
                 Console.Write("Check-out date (dd/MM/yyyy): ");
                 checkOut = DateTime.Parse(Console.ReadLine());
 
-                String error = reservation.UpdateDates(checkIn, checkOut);
+                reservation.UpdateDates(checkIn, checkOut);
 
-                if (error != null)
-                {
-                    Console.WriteLine("Error in reservation: " + error);
-                }
-
-                else
-                {
-                    Console.WriteLine("Reservation: " + reservation);
-                }
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
             }
         }
     }
